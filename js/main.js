c187b62d8fadaa557749225808f8590067f97bc4
Alexander <yaltasashay@gmail.com>
2017-05-06 03:31:17 +0300

    menu done

--- js/main.js
+++ js/main.js
@@ -25,3 +25,12 @@ window.onload = function () {
 	// $('.div_td').slick();
 	});
 }
+
+
+ $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
