a383941e2a8bc7946084dd9cdc2219104b7bba38
Alexander <yaltasashay@gmail.com>
2017-05-28 23:12:40 +0300

    2.0

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
 "use strict";
 
+
+// Слайдер для фото 
 window.onload = function () {
 	jQuery(document).ready(function() {
-		// $('#slider').slick({
-		//   	lazyLoad: 'ondemand',
-  // 			slidesToShow: 3,
-  // 			slidesToScroll: 1
-		// });
-
 		$('#slider').slick({
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 2000,
-	});
-
-
-// 		$('#slider').slick({
-	//   infinite: true,
-	//   slidesToShow: 3,
-	//   slidesToScroll: 3
-	// });
-
-	// $('.div_td').slick();
+	 		slidesToShow: 3,
+	  		slidesToScroll: 1,
+	  		autoplay: true,
+	  		autoplaySpeed: 2000,
+		});
 	});
 }
 
-
- $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
+// Анимация выхода логотипа
+$(document).on('click', 'a.page-scroll', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+// Быстрая кнопка наверх
+$(document).ready(function(){ 
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	});
+
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+			return false;
+	});
+});
